0d5e6ca0a07969293a3cc622666a847c7467347c
Lucas Vilela <lucasvilela91@msn.com>
2026-02-09 23:18:33 -0300

    chore: initial setup

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.jsx to edit this screen.</Text>
+    </View>
+  );
+}
+
