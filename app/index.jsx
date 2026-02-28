0376f0862e5a53d99963c7e3e3330eb144beee1e
Lucas Vilela <lucasvilela91@msn.com>
2026-02-27 21:20:32 -0300

    feat: Adds dynamic timer selection with improved UI

--- app/index.jsx
+++ app/index.jsx
@@ -1,28 +1,59 @@
-import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import { useState } from 'react';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+
+const pomodoro = [
+  {
+    id: 'focus',
+    initialValue: 25,
+    image: require('./pomodoro.png'),
+    display: 'Foco',
+  },
+  {
+    id: 'short',
+    initialValue: 5,
+    image: require('./short.png'),
+    display: 'Pausa curta',
+  },
+  {
+    id: 'long',
+    initialValue: 15,
+    image: require('./long.png'),
+    display: 'Pausa longa',
+  },
+];
 
 export default function Index() {
+  const [timeType, setTimeType] = useState(pomodoro[1]);
   return (
-    <View
-      style={styles.container}
-    >
-      <Image source={require('./pomodoro.png')}/>
+    <View style={styles.container}>
+      <Image source={timeType.image} />
       <View style={styles.actions}>
+        <View style={styles.context}>
+          {pomodoro.map((p) => (
+            <Pressable
+              key={p.id}
+              style={ timeType.id === p.id ? styles.contextButtonActive : null }
+              onPress={() => setTimeType(p)}
+            >
+              <Text style={styles.contextButtonText}>{p.display}</Text>
+            </Pressable>
+          ))}
+        </View>
         <Text style={styles.timer}>
-          25:00
+          {new Date(timeType.initialValue * 60 * 1000).toLocaleTimeString(
+            'pt-BR',
+            { minute: '2-digit', second: '2-digit' },
+          )}
         </Text>
         <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>
-            Começar
-          </Text>
+          <Text style={styles.buttonText}>Começar</Text>
         </Pressable>
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais.
         </Text>
-        <Text style={styles.footerText}>
-          Desenvolvido por Alura. 
-        </Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
       </View>
     </View>
   );
@@ -31,10 +62,10 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#021123',
-    gap: 40
+    gap: 40,
   },
   actions: {
     paddingVertical: 24,
@@ -44,23 +75,37 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     borderWidth: 2,
     borderColor: '#144480',
-    gap: 32
+    gap: 32,
+  },
+  context: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  contextButtonActive: {
+    backgroundColor: '#144480',
+    borderRadius: 8,
+  },
+  contextButtonText: {
+    fontSize: 12.5,
+    color: '#FFF',
+    padding: 8,
   },
   timer: {
     fontSize: 54,
     color: '#FFF',
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   button: {
     backgroundColor: '#B872FF',
     borderRadius: 32,
-    padding: 8
+    padding: 8,
   },
   buttonText: {
     textAlign: 'center',
     color: '#021123',
-    fontSize: 18
+    fontSize: 18,
   },
   footer: {
     width: '80%',
@@ -68,6 +113,6 @@ const styles = StyleSheet.create({
   footerText: {
     textAlign: 'center',
     color: '#98A0A8',
-    fontSize: 12.5
-  }
-})
+    fontSize: 12.5,
+  },
+});
