61f479259861c5192fe8c1a92577e623c195dc84
Lucas Vilela <lucasvilela91@msn.com>
2026-03-04 22:17:06 -0300

    feat: Improves timer logic and adds play/pause icons

--- app/index.jsx
+++ app/index.jsx
@@ -1,71 +1,103 @@
-import { useRef, useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { FokusButton } from '../components/FokusButton';
-import { ActionButton } from '../components/ActionButton';
-import { Timer } from '../components/Timer';
+import { useRef, useState } from "react";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import { FokusButton } from "../components/FokusButton";
+import { ActionButton } from "../components/ActionButton";
+import { Timer } from "../components/Timer";
+import { IconPause, IconPlay } from "../components/Icons";
 
 const pomodoro = [
   {
     id: 'focus',
-    initialValue: 25,
+    initialValue: 25 * 60,
     image: require('./pomodoro.png'),
-    display: 'Foco',
+    display: 'Foco'
   },
   {
     id: 'short',
-    initialValue: 5,
+    initialValue: 5 * 60,
     image: require('./short.png'),
-    display: 'Pausa curta',
+    display: 'Pausa curta'
   },
   {
     id: 'long',
-    initialValue: 15,
+    initialValue: 15 * 60,
     image: require('./long.png'),
-    display: 'Pausa longa',
+    display: 'Pausa longa'
   },
-];
+]
 
 export default function Index() {
-  const [timeType, setTimeType] = useState(pomodoro[1]);
 
-  const timerRef = useRef(null);
+  const [timerType, setTimerType] = useState(pomodoro[0])
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
+  const [timerRunning, setTimerRunning] = useState(false)
 
-  const toogleTime = () => {
-    if (timerRef.current) {
-      // pausar
-      clearInterval(timerRef.current);
-      return;
+  const timerRef = useRef(null)
+
+  const clear = () => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current)
+      timerRef.current = null
+      setTimerRunning(false)
     }
+  }
+
+  const toggleTimerType = (newTimerType) => {
+    setTimerType(newTimerType)
+    setSeconds(newTimerType.initialValue)
+    clear()
+  }  
+
+  const toggleTimer = () => {
+    if (timerRef.current) {
+      clear()
+      return
+    }
+    
+    setTimerRunning(true)
+
     const id = setInterval(() => {
-      console.log('timer rolando');
-    }, 1000);
-    timerRef.current = id;
-  };
+      setSeconds(oldState => {
+        if (oldState === 0) {
+          clear()
+          return timerType.initialValue
+        }
+        return oldState - 1
+      })
+    }, 1000)
+    timerRef.current = id
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={timeType.image} />
+    <View
+      style={styles.container}
+    >
+      <Image source={timerType.image}/>
       <View style={styles.actions}>
         <View style={styles.context}>
-          {pomodoro.map((p) => (
-            <ActionButton
+          {pomodoro.map(p => (
+            <ActionButton 
               key={p.id}
-              active={timeType.id === p.id}
-              onPress={() => setTimeType(p)}
+              active={ timerType.id === p.id }
+              onPress={() => toggleTimerType(p)}
               display={p.display}
             />
           ))}
         </View>
-        <Timer totalSeconds={timeType.initialValue} />
+        <Timer totalSeconds={seconds} />
         <FokusButton 
-        title={timerRef.current ? 'Pausar' : 'Começar'}
-        onPress={toogleTime} 
+          title={timerRunning ? 'Pausar' : 'Começar'}
+          icon={timerRunning ? <IconPause /> : <IconPlay />}
+          onPress={toggleTimer}
         />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais.
         </Text>
-        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura. 
+        </Text>
       </View>
     </View>
   );
@@ -74,10 +106,10 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: '#021123',
-    gap: 40,
+    gap: 40
   },
   actions: {
     paddingVertical: 24,
@@ -87,18 +119,12 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     borderWidth: 2,
     borderColor: '#144480',
-    gap: 32,
+    gap: 32
   },
   context: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  timer: {
-    fontSize: 54,
-    color: '#FFF',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    alignItems: 'center'
   },
   footer: {
     width: '80%',
@@ -106,6 +132,6 @@ const styles = StyleSheet.create({
   footerText: {
     textAlign: 'center',
     color: '#98A0A8',
-    fontSize: 12.5,
-  },
-});
+    fontSize: 12.5
+  }
+})
